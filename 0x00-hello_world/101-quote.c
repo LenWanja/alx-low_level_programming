91fc3637ae2169a1922f9e89c3b73ab2ab14309e
LenWanja <wanjah559@gmail.com>
2022-06-30 15:42:58 -0700

    8 Quote done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" -Dora Korpar, 2015-10-19"));
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
