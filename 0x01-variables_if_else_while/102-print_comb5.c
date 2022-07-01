0bcdc17cc6b297e714e8c177729bde4890fdc7f0
LenWanja <wanjah559@gmail.com>
2022-07-01 07:02:54 -0700

    Print 12 done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,6 +11,7 @@ int i, j;
 for (i = 0; i != 100; i++)
 for (j = 0; j != 100; j++)
 if (j > i)
+{
 putchar((i / 10) + 48);
 putchar((i % 10) + 48);
 putchar(32);
@@ -20,6 +21,7 @@ if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(32);
+}
 }
 putchar('\n');
 return (0);
