7cd6f5d2a8a1a861e8fe96f318bdaaa0399e693e
LenWanja <wanjah559@gmail.com>
2022-07-05 03:45:58 -0700

    0_putchar done

--- 0x02-fuctions_nested_loops/0-putchar.c
+++ 0x02-fuctions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
-#include "putchar.h"
+#include <main.h>
 /**
-* main - Prints putchar
-*
-* Return: Always 0 (Success)
+* main - prints putchar
+* Return: Always 0
 */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 return (0);
 }
