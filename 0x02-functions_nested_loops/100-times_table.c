8c20b8609b9054a68d536f1b6652a7dcc364a55f
LenWanja <wanjah559@gmail.com>
2022-07-05 13:03:34 -0700

    Times 10 done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - main block
-* Description: computes and prints the sum of all the multiples of 3 or
-* 5 below 1024 (excluded), followed by a new line
+* main - print sum of multiples of 3 or 5 below 1024
 * Return: 0
 */
 int main(void)
 {
-int c = 0;
-int sum = 0;
-while (c < 1024)
+int n;
+int sum;
+sum = 0;
+for (n = 0; n < 1024; n++)
 {
-if (c % 3 == 0 || c % 5 == 0)
+if ((n % 3 == 0 || (n % 5 == 0))
 {
-sum += c;
+sum += n;
 }
-c++;
 }
-printf("%i\n", sum);
+printf("%d\n", sum);
 return (0);
 }
