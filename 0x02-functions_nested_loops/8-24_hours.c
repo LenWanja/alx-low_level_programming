d82742ed200918f22481d36fa63da4b8ccd3799b
LenWanja <wanjah559@gmail.com>
2022-07-05 11:00:34 -0700

    Hours 8 fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,35 +1,44 @@
 #include "main.h"
 /**
 * jack_bauer -> prints 24 hours
+* Return:void
 */
 void jack_bauer(void)
 {
-int i, j;
-for (i = 0; i < 24; i++)
+int h1;
+int h2;
+int m1;
+int m2;
+int a = 9;
+h2 = 0;
+while (h2 <= 2)
 {
-for (j = 0; j < 60; j++)
+if (h2 == 2)
 {
-if (i < 10)
-{_putchar('0');
-_putchar(i + '0');
+/*Restrain to 23h, not 29*/
+a = 3;
 }
-else if (i >= 10)
+h1 = 0;
+while (h1 <= a)
 {
--putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-}
-if (j < 10)
+m2 = 0;
+while (m2 <= 5)
 {
-_putchar (':');
-_putchar('0');
-_putchar('j  + '0');
-}
-else if (j > 10)
+m1 = 0;
+while (m1 <= 9)
 {
-_putchar( ':' );
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
+_putchar('0' + h2);
+_putchar('0' + h1);
+_putchar(':');
+_putchar('0' + m2);
+_putchar('0' + m1);
+_putchar('\n');
+m1++;
 }
+m2++;
 }
+h1++;
+}
+h2++;
 }
 }
