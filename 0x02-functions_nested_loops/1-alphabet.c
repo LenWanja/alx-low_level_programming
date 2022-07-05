cbd62275686b0a7d86e87b5ad0787ff996307c2f
LenWanja <wanjah559@gmail.com>
2022-07-05 05:12:48 -0700

    Alphabet 1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_alphabet -> prints the lowercase alphabets
+*/
+void print_alphabet(void)
+{
+int j;
+for (j = 'a'; j <= 'z'; j++
+{
+_putchar(j);
+}
+_putchar('\n');
+}
