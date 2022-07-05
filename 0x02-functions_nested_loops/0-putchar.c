cbbf86cc2064aecea1162ef11efa1369f90b2cbe
LenWanja <wanjah559@gmail.com>
2022-07-05 04:11:36 -0700

    0 putchar done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "putchar.h"
 /**
 * main - Prints putchar
 *
@@ -14,6 +14,6 @@ _putchar("c");
 _putchar("h");
 _putchar("a");
 _putchar("r");
-_putchar('\n');
+_putchar("\n");
 return (0);
 }
