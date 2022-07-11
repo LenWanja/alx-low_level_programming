86e7d7891daf8f25907dcde42007911c92f37270
LenWanja <wanjah559@gmail.com>
2022-07-11 11:32:51 -0700

    Commits 4,5,6 done

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,8 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char n);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
 
 #endif /* MAIN_H */
