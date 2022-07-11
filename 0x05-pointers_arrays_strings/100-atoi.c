d05b3b85a8013485b05f78d3bff2f11d7fcbb945
LenWanja <wanjah559@gmail.com>
2022-07-11 12:22:49 -0700

    Advanced hot fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,101 @@
 #include "main.h"
 
 /**
-* _atoi - convert a string to an integer.
-* @s: char type string
-* Return: integer converted
+* _strlen - returns the length of a string
+* @s: string
+* Return: length
+*/
+
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s != '\0')
+{
+len++;
+s++;
+}
+
+return (len);
+}
+
+/**
+* idx_num_starts - find index where a digit is first found in string
+* @s: string to search
+* Return: integer index where digit is first found
+*/
+
+int idx_num_starts(char *s)
+{
+int i;
+
+for (i = 0; i < _strlen(s); i++)
+{
+if (s[i] >= '0' && s[i] <= '9')
+return (i);
+}
+return (-1); /* return -1 if no digits found */
+}
+
+/**
+* find_sign - determine if integer is negative
+* @s: integer
+* Return: integer 1 or -1
+*/
+int find_sign(char *s)
+{
+int negatives = 0, i = 0, sign = 1;
+
+while (i < (idx_num_starts(s)))
+{
+if (s[i++] == '-')
+negatives++;
+}
+if (negatives % 2 != 0)
+sign = -1;
+return (sign);
+}
+
+/**
+* _atoi - convert string to int
+* @s: string to convert
+* Return: integer
 */
 
 int _atoi(char *s)
 {
-int sign = 1, resp = 0, firstNum;
 
-for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+int idx_digit_starts = (idx_num_starts(s));
+int sign;
+int digits_to_print = 0;
+int t = 1, i;
+unsigned int num = 0;
+int digit = (idx_num_starts(s));
+
+if (idx_digit_starts < 0) /* if no digits found, exit program */
+return (0);
+
+sign = find_sign(s);
+
+while ((s[idx_digit_starts] >= '0' && s[idx_digit_starts] <= '9')
+&& (idx_digit_starts <= _strlen(s))) /* count digits to print */
 {
-if (s[firstNum] == '-')
-{
-sign *= -1;
-}
+digits_to_print += 1;
+idx_digit_starts++;
 }
 
-for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+i = 1;
+while (i < digits_to_print) /* find powers of ten to multiply places */
 {
-resp *= 10;
-resp += (s[i] - 48);
+t *= 10;
+i++;
 }
 
-return (sign *resp);
+for (i = digit; i < (digit + digits_to_print); i++) /* calculate num */
+{
+num += (s[i] - '0') * t;
+t /= 10;
+}
+
+return (num *sign);
 }
