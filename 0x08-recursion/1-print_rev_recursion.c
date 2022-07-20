2c8c7984ab1d90eed07a98beceb88a51d655757e
LenWanja <wanjah559@gmail.com>
2022-07-20 11:05:59 -0700

    Hot fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,9 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s > '\0')
-	{
-		 _print_rev_recursion(s + 1);
-		 _putchar(*s);
-	}
+if (*s == '\0')
+return;
+_print_rev_recursion(s + 1);
+_putchar(*s);
+
 }
