57deb44c95f23481bfe9999f9f796f15d6e14d1f
LenWanja <wanjah559@gmail.com>
2022-07-25 04:52:02 -0700

    Mandatory tasks commits

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - free the grid
+ * @grid: grid of memories
+ * @height: int
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+int i = 0;
+for (; i < height; i++)
+	free(grid[i]);
+free(grid);
+}
