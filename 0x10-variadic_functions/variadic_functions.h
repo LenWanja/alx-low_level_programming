0e53caf756ba586d1bf922215c50bd30a9cee5df
LenWanja <wanjah559@gmail.com>
2022-08-04 06:00:08 -0700

    All tasks commits

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+
+#endif /* VARIADIC_FUNCTIONS */
